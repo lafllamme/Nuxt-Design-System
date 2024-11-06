1099fbf09b71f25cb2262a49e3e70cf2d3bba0dc
Lafllamme <teke900@gmail.com>
2024-11-06 20:57:03 +0100

    fix: Correct file path references in clean.sh script
    Updated the script to correctly reference lock files in the project root directory instead of the target directory. Added a note indicating that the script should be run from the project's root.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Define the relative path to the target directory
+# Needs to be run from the root of the project !
 TARGET_DIR="./app"
 
 # Delete node_modules folder if it exists in the target directory
@@ -36,27 +37,27 @@ else
 fi
 
 # Delete package-lock.json file if it exists in the target directory
-if [ -f "$TARGET_DIR/package-lock.json" ]; then
+if [ -f "package-lock.json" ]; then
   echo "Deleting package-lock.json in $TARGET_DIR..."
-  rm -f "$TARGET_DIR/package-lock.json"
+  rm -f "package-lock.json"
 else
   echo "package-lock.json does not exist in $TARGET_DIR."
 fi
 
 # Delete bun.lockb file if it exists in the target directory
-if [ -f "$TARGET_DIR/bun.lockb" ]; then
-  echo "Deleting bun.lockb in $TARGET_DIR..."
-  rm -f "$TARGET_DIR/bun.lockb"
+if [ -f "bun.lockb" ]; then
+  echo "Deleting bun.lockb in root..."
+  rm -f "bun.lockb"
 else
-  echo "bun.lockb does not exist in $TARGET_DIR."
+  echo "bun.lockb does not exist in root."
 fi
 
 # Delete pnpm-lock.yaml file if it exists in the target directory
-if [ -f "$TARGET_DIR/pnpm-lock.yaml" ]; then
-  echo "Deleting pnpm-lock.yaml in $TARGET_DIR..."
-  rm -f "$TARGET_DIR/pnpm-lock.yaml"
+if [ -f "pnpm-lock.yaml" ]; then
+  echo "Deleting pnpm-lock.yaml in root..."
+  rm -f "pnpm-lock.yaml"
 else
-  echo "pnpm-lock.yaml does not exist in $TARGET_DIR."
+  echo "pnpm-lock.yaml does not exist in root."
 fi
 
 echo "Folders and lock files deleted successfully."
