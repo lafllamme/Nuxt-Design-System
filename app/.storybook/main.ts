4391a5cbfcb291e60f7d1e597138c702cdd663e9
Lafllamme <teke900@gmail.com>
2024-11-06 20:59:14 +0100

    feat: add Storybook example components and configurations
    Added various example components, including welcome, button, and header, with corresponding CSS styling and configuration in Storybook. Also included an MDX file with detailed instructions for setting up and using Storybook.

---
+++ app/.storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook-vue/nuxt";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-interactions",
+    "@storybook/addon-essentials",
+
+  ],
+  framework: {
+    name: "@storybook-vue/nuxt",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  vite: {
+    optimizeDeps: {
+      include: ['storybook > @storybook/core > jsdoc-type-pratt-parser'],
+    },
+  },
+};
+export default config;
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-/*
-  modules: ['@nuxtjs/storybook']
-*/
+  modules: ['@nuxtjs/storybook'],
+  vite: {
+    optimizeDeps: {
+      include: ['storybook > @storybook/core > jsdoc-type-pratt-parser'],
+    },
+  },
 })
