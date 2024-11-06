3fd4e2de17c3102e3f9531f93c61d6da3a95c1ee
Lafllamme <teke900@gmail.com>
2024-11-06 23:40:29 +0100

    feat: integrate UnoCSS package and module
    Added UnoCSS as a dependency and included it in the Nuxt configuration modules. This integration enhances the styling options and utility-first design of the project.

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook'],
-  vite: {
-    optimizeDeps: {
-      include: ['storybook > @storybook/core > jsdoc-type-pratt-parser'],
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: [
+        '@nuxtjs/storybook',
+        '@unocss/nuxt',
+
+    ],
+    vite: {
+        optimizeDeps: {
+            include: ['storybook > @storybook/core > jsdoc-type-pratt-parser'],
+        },
     },
-  },
 })
