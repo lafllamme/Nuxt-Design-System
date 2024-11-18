1f9a8010710019401065cac19eb48df61357d73b
Lafllamme <teke900@gmail.com>
2024-11-19 00:21:03 +0100

    feat: add default template & dynamic theme switching in Storybook
    Add support for dynamic theme switching with light and dark modes using a new toolbar in Storybook. Refactor the preview configuration to include global theme types and update the decorators to apply the selected theme.

--- app/.storybook/preview.ts
+++ app/.storybook/preview.ts
@@ -1,18 +1,70 @@
 import type { Preview } from "@storybook/vue3";
-import 'virtual:uno.css'
-// @ts-ignore: Unused import
-import '~/assets/uno/reset'; // Importing the file directly applies the styles
+import 'virtual:uno.css';
+import '~/assets/uno/reset';
+import DefaultLayout from '~/stories/layouts/DefaultLayout.vue';
+import { theme } from "./theme";
+import '../assets/fonts/storybook.scss';
+
+const applyTheme = (theme: string) => {
+    const html = document.documentElement;
+    if (theme === 'dark') {
+        html.classList.add('dark-theme');
+    } else {
+        html.classList.remove('dark-theme');
+    }
+};
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+        options: {
+            storySort: {
+                method: 'alphabetical',
+                order: [],
+                locales: '',
+            },
+        },
     },
-  },
+    globalTypes: {
+        theme: {
+            description: 'Global theme for components',
+            toolbar: {
+                title: 'Theme',
+                icon: 'circle', // Initial icon, will be replaced dynamically
+                items: [
+                    { value: 'light', title: 'Light Mode', icon: 'sun' },
+                    { value: 'dark', title: 'Dark Mode', icon: 'moon' },
+                ],
+                dynamicTitle: true,
+            },
+        },
+    },
+    initialGlobals: {
+        theme: 'light',
+    },
+    docs: {
+        theme: theme,
+    },
+    layout: 'fullscreen',
+    decorators: [
+        (story, context) => {
+            const { globals } = context;
+            applyTheme(globals.theme);
+            return {
+                components: { story, DefaultLayout },
+                template: `
+          <DefaultLayout>
+            <story/>
+          </DefaultLayout>`,
+            };
+        },
+    ],
 };
 
 export default preview;
