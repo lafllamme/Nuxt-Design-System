7d78f4b80a8ac1d28688da7aec799b254f9ea178
Lafllamme <teke900@gmail.com>
2024-11-19 00:21:29 +0100

    feat: add custom theme for Storybook
    Create a new custom theme for Storybook, including specific typography, colors, and brand settings. This change enhances the visual consistency and branding of the design system documentation.

---
+++ app/.storybook/theme.ts
@@ -0,0 +1,40 @@
+import {create} from '@storybook/theming/create';
+
+export const theme = create({
+    base: 'light',
+    // Typography
+    fontBase: '"Inter", sans-serif', // Matches $font-body from Sass
+    fontCode: '"Fira Code", monospace',
+
+    brandTitle: 'Plant.me Design System',
+    brandUrl: '#',
+    brandImage: 'https://i.imgur.com/aWwKWZj.png',
+    brandTarget: '#',
+
+    //
+    colorPrimary: '#3A10E5',
+    colorSecondary: '#6e6666',
+
+    // UI
+    appBg: '#FEFDFC',
+    appContentBg: '#ffffff',
+    appPreviewBg: '#ffffff',
+    appBorderColor: '#BBBBBB',
+    appBorderRadius: 4,
+
+    // Text colors
+    textColor: '#1D3B31',
+    textInverseColor: '#ffffff',
+
+    // Toolbar default and active colors
+    barTextColor: '#9E9E9E',
+    barSelectedColor: '#646464',
+    barHoverColor: '#646464',
+    barBg: '#ffffff',
+
+    // Form colors
+    inputBg: '#ffffff',
+    inputBorder: '#1D3B31',
+    inputTextColor: '#1D3B31',
+    inputBorderRadius: 2,
+});
