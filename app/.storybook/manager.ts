4537ba6d53e03b9368abc104aaf8b53cd704f0a4
Lafllamme <teke900@gmail.com>
2024-11-19 00:21:42 +0100

    feat: add custom Storybook manager configuration
    Introduce a new Storybook manager configuration file to tailor the Storybook interface. This setup customizes navigation, panel sizes, shortcut enabling, and sidebar and toolbar visibility for a more organized and efficient UI.

---
+++ app/.storybook/manager.ts
@@ -0,0 +1,27 @@
+import {addons} from '@storybook/manager-api';
+import { themes } from '@storybook/theming';
+import {theme} from './theme';
+
+// @ts-ignore
+addons.setConfig({
+    navSize: 300,
+    bottomPanelHeight: 300,
+    rightPanelWidth: 300,
+    panelPosition: 'bottom',
+    enableShortcuts: true,
+    showToolbar: true,
+    theme: theme,
+    selectedPanel: undefined,
+    initialActive: 'sidebar',
+    sidebar: {
+        showRoots: true,
+        collapsedRoots: ['other'],
+    },
+    toolbar: {
+        title: {hidden: false},
+        zoom: {hidden: false},
+        eject: {hidden: false},
+        copy: {hidden: false},
+        fullscreen: {hidden: false},
+    },
+});
