b5397b84b453b1eee2f0e27ac4eeb6affe76a945
Lafllamme <teke900@gmail.com>
2024-12-03 22:40:45 +0100

    feat: streamline PopOver component for cleaner markup
    Refactored the PopOver component to reduce code duplication and improve readability by condensing class attributes. Converted the script setup to a more concise format without changing functionality. These changes aim to maintain the component's visual appearance while making the codebase more maintainable.

--- app/stories/components/PopOver/PopOver.vue
+++ app/stories/components/PopOver/PopOver.vue
@@ -1,45 +1,21 @@
 <!-- Popover.vue -->
-<script setup lang="ts">
-import {
-  PopoverArrow,
-  PopoverClose,
-  PopoverContent,
-  PopoverPortal,
-  PopoverRoot,
-  PopoverTrigger,
-} from 'radix-vue';
+<script lang="ts" setup>
+import { PopoverArrow, PopoverClose, PopoverContent, PopoverPortal, PopoverRoot, PopoverTrigger } from 'radix-vue'
 </script>
 
 <template>
   <div
-      class="
-      flex items-center justify-center relative w-full p-4 rounded-t-lg
-      bg-gradient-to-br from-teal-9 to-green-9
-    "
+    class="relative w-full flex items-center justify-center rounded-t-lg from-teal-9 to-green-9 bg-gradient-to-br p-4"
   >
     <PopoverRoot>
       <PopoverTrigger
-          class="
-          inline-flex items-center justify-center
-          h-[35px] px-[15px] py-0
-          rounded-[4px] text-[15px] leading-[1] font-medium
-          !bg-white text-violet-11
-          shadow-[0_2px_10px_var(--blackA-7)]
-          hover:bg-mauve-3 focus:shadow-[0_0_0_2px_black]
-        "
+        class="h-[35px] inline-flex items-center justify-center rounded-[4px] px-[15px] py-0 text-[15px] text-violet-11 font-medium leading-[1] shadow-[0_2px_10px_var(--blackA-7)] !bg-white hover:bg-mauve-3 focus:shadow-[0_0_0_2px_black]"
       >
         More info
       </PopoverTrigger>
       <PopoverPortal>
         <PopoverContent
-            class="
-            rounded-[4px] p-[20px] w-[260px]
-            text-[15px] leading-[1] text-violet-11
-            bg-white
-            shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]
-            focus:outline-none
-            focus:shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px,0_0_0_2px_var(--violet-7)]
-          "
+          class="w-[260px] rounded-[4px] bg-white p-[20px] text-[15px] text-violet-11 leading-[1] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px,0_0_0_2px_var(--violet-7)] focus:outline-none"
         >
           Some more info...
           <PopoverClose />
