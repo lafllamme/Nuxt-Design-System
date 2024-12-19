1c345caa4c0a31f83a813a507a1797274be9cb91
Lafllamme <teke900@gmail.com>
2024-12-19 22:49:45 +0100

    feat: Add logging functionality to entrypoint script
    Introduced a `log_message` function to format and display logs with timestamps and colored text. Replaced plain echo statements with structured log messages to improve clarity and consistency in script outputs.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Define color codes
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+
+# Function to print logs in the desired format
+log_message() {
+  #entry="$1"
+  message="$2"
+  date=$(date '+%Y-%m-%d %H:%M:%S')
+
+  echo  "${BLUE}[Entrypoint]${RESET} | ${YELLOW}(${date})${RESET}: $message"
+}
+
 # Change to the app directory
 cd /app || exit
 
@@ -10,21 +24,21 @@ command_exists() {
 
 # Check if package.json exists
 if [ -f package.json ]; then
-  echo "package.json found."
+  log_message "package.json check" "package.json found."
 
   if command_exists pnpm; then
-    echo "pnpm is available. Installing dependencies and starting the development server..."
+    log_message "pnpm" "pnpm is available. Installing dependencies and starting the development server..."
     pnpm install
     pnpm run dev
   elif command_exists bun; then
-    echo "bun is available. Installing dependencies and starting the development server..."
+    log_message "bun" "bun is available. Installing dependencies and starting the development server..."
     bun install
     bun run dev
   else
-    echo "Neither pnpm nor bun is available. Exiting."
+    log_message "error" "Neither pnpm nor bun is available. Exiting."
     exit 1
   fi
 else
-  echo "No package.json found in /app. Starting a shell to initialize the project..."
+  log_message "error" "No package.json found in /app. Starting a shell to initialize the project..."
   tail -f /dev/null
 fi
