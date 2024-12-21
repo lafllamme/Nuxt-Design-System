00b0e2a74c039792076b43712b91f3663b0f861e
Lafllamme <teke900@gmail.com>
2024-12-21 13:53:03 +0100

    feat: Refactor nuxt.config.ts for improved readability
    Reformatted the configuration file by adjusting indentation and removing unnecessary comments. This change enhances code clarity and maintains functionality without altering any settings."

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,29 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-04-03',
-    devtools: {enabled: true},
-    nitro: {
-        logLevel: 'debug',
-    },
-    typescript
-        : {
-        typeCheck
-            : true
-    },/*
-    vite: {
-        server: {
-            hmr: {
-                protocol: "http",
-                host: "localhost",
-                clientPort: 3000,
-                port: 3000,
-            },
-        },
-    },*/
-    modules: [
-      '@nuxtjs/storybook',
-      '@unocss/nuxt',
-      'radix-vue/nuxt',
-      '@nuxtjs/color-mode',
-    ],
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  nitro: {
+    logLevel: 'debug',
+  },
+  typescript: {
+    typeCheck: true,
+  },
+  modules: [
+    '@nuxtjs/storybook',
+    '@unocss/nuxt',
+    'radix-vue/nuxt',
+    '@nuxtjs/color-mode',
+  ],
 })
