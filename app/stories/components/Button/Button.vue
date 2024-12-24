9ef92927509c4a416bf6e6849f50fcc1fb7dfb62
Lafllamme <teke900@gmail.com>
2024-12-24 03:27:41 +0100

    fix: correct script tag placement and improve import formatting
    Reorganized the `<script>` tag placement to match conventions and fixed formatting for type imports. These changes enhance code clarity and maintainability without altering functionality.

--- app/stories/components/Button/Button.vue
+++ app/stories/components/Button/Button.vue
@@ -1,27 +1,28 @@
 <!-- components/Button/Button.vue -->
 
-<template>
-  <button :class="buttonClasses" :disabled="disabled">
-    {{ label }}
-  </button>
-</template>
-
-<script setup lang="ts">
-import { ButtonProps, buttonDefaults } from './Button.model';
+<script lang="ts" setup>
+import type { ButtonProps } from './Button.model'
+import { buttonDefaults } from './Button.model'
 
 // Define props using the ButtonProps interface
 const props = withDefaults(
-    defineProps<ButtonProps>(),
-    buttonDefaults
-);
+  defineProps<ButtonProps>(),
+  buttonDefaults,
+)
 
 // Compute button classes based on props
 const buttonClasses = computed(() => [
   `btn-${props.type}`,
   `size-${props.size}`,
   { 'btn-disabled': props.disabled },
-]);
+])
 </script>
+
+<template>
+  <button :class="buttonClasses" :disabled="disabled">
+    {{ label }}
+  </button>
+</template>
 
 <style scoped>
 
