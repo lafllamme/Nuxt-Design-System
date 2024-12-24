bb659d17a3ee6dc534b32c205d9dab7de13508d7
Lafllamme <teke900@gmail.com>
2024-12-24 03:23:36 +0100

    feat: implement utilities with cslx
    Replaced static class definitions with dynamic `useClsx` calls for better readability and maintainability. This change improves modularity by organizing CSS classes and enabling conditional styling more effectively. No functional changes were introduced.

--- app/stories/components/PopOver/PopOver.vue
+++ app/stories/components/PopOver/PopOver.vue
@@ -5,17 +5,35 @@ import { PopoverArrow, PopoverClose, PopoverContent, PopoverPortal, PopoverRoot,
 
 <template>
   <div
-    class="relative w-full flex items-center justify-center rounded-t-lg from-teal-9 to-green-9 bg-gradient-to-br p-4"
+    :class="
+      useClsx(
+        'relative w-full py-24 flex items-center justify-center p-4',
+        'rounded-t-lg from-teal-9 to-green-9 bg-gradient-to-br',
+      )"
   >
     <PopoverRoot>
       <PopoverTrigger
-        class="h-[35px] inline-flex items-center justify-center rounded-[4px] px-[15px] py-0 text-[15px] text-violet-11 font-medium leading-[1] shadow-[0_2px_10px_var(--blackA-7)] !bg-white hover:bg-mauve-3 focus:shadow-[0_0_0_2px_black]"
+        :class="
+          useClsx(
+            'h-[35px] inline-flex items-center justify-center py-0 px-[15px]',
+            'font-[-apple-system] text-[15px] text-violet-11 font-500 leading-[1]',
+            'rounded-[4px] px-[15px]',
+            'shadow-[0_2px_10px_var(--un-preset-radix-black7A)] !bg-white',
+            'hover:bg-mauve-3 focus:shadow-[0_0_0_2px] focus:shadow-black',
+          )"
       >
         More info
       </PopoverTrigger>
       <PopoverPortal>
         <PopoverContent
-          class="w-[260px] rounded-[4px] bg-white p-[20px] text-[15px] text-violet-11 leading-[1] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px,0_0_0_2px_var(--violet-7)] focus:outline-none"
+          :class="
+            useClsx(
+              'w-[260px] rounded-[4px] bg-white p-[20px]',
+              'text-[15px] text-violet-11 font-400 leading-[1]',
+              'shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]',
+              'focus:outline-none hue-violet',
+              'focus:shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px,0_0_0_2px_var(--un-preset-radix-hue7)]',
+            )"
         >
           Some more info...
           <PopoverClose />
@@ -25,5 +43,4 @@ import { PopoverArrow, PopoverClose, PopoverContent, PopoverPortal, PopoverRoot,
     </PopoverRoot>
   </div>
 </template>
-
 <!-- No <style> block needed since we're using UnoCSS classes -->
