82b2d4caa18206bc732c3134b060b68e5fcf48c5
Lafllamme <teke900@gmail.com>
2024-12-24 03:25:04 +0100

    feat: add auto import for consola
    Enable automatic import of Consola to streamline logging functionality. This change improves code readability and reduces manual imports across the project.

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  imports: {
+    // Auto-import
+    imports: [
+      {
+        from: 'consola',
+        name: 'default',
+        as: 'consola',
+      },
+    ],
+  },
   nitro: {
     logLevel: 'debug',
   },
