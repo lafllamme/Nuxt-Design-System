53f639a707a13c251a6be025d2d47c0c4329b308
Lafllamme <teke900@gmail.com>
2024-12-24 03:26:19 +0100

    fix: adjust ColorPalette stories for consistent formatting
    Updated code formatting in ColorPalette stories file for improved readability and consistency. Removed unnecessary comments and ensured adherence to style guidelines without altering functionality."

--- app/stories/patterns/Palette/ColorPlatte.stories.ts
+++ app/stories/patterns/Palette/ColorPlatte.stories.ts
@@ -1,43 +1,40 @@
-// @ts-ignore
-import type {Meta, StoryObj} from '@storybook/vue3'
-import {allPalettes, defaults} from './ColorPalette.model'
-import type {ColorPaletteProps} from './ColorPalette.model'
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { allPalettes, defaults } from './ColorPalette.model'
 import ColorPalette from './ColorPalette.vue'
 
 const meta: Meta<typeof ColorPalette> = {
-    title: 'Palettes/ColorPalette',
-    component: ColorPalette,
-    tags: ['autodocs'],
-    argTypes: {
-        palette: {
-            control: {
-                type: 'select',
-                options: allPalettes,
-            },
-            description: 'Choose a color palette',
-        }
-    }
+  title: 'Palettes/ColorPalette',
+  component: ColorPalette,
+  tags: ['autodocs'],
+  argTypes: {
+    palette: {
+      control: {
+        type: 'select',
+        options: allPalettes,
+      },
+      description: 'Choose a color palette',
+    },
+  },
 
 } satisfies Meta<typeof ColorPalette>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {
-        palette: defaults.palette,
-    },
-};
+  args: {
+    palette: defaults.palette,
+  },
+}
 
 export const mauve: Story = {
-    args: {
-        palette: 'mauve',
-    },
+  args: {
+    palette: 'mauve',
+  },
 }
 
 export const red: Story = {
-    args: {
-        palette: 'red',
-    },
+  args: {
+    palette: 'red',
+  },
 }
-
