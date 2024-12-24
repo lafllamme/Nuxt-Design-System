1cf75318e281a2d833a371cb856202e737fa8559
Lafllamme <teke900@gmail.com>
2024-12-24 03:26:59 +0100

    fix: Add 'black' and 'white' to available color palettes
    Extended the palette type and its associated arrays to include 'black' and 'white'. This ensures broader support for color options throughout the application. Adjusted formatting for better readability.

--- app/stories/patterns/Palette/ColorPalette.model.ts
+++ app/stories/patterns/Palette/ColorPalette.model.ts
@@ -1,21 +1,49 @@
 export type palette =
-    'gray' | 'mauve' | 'slate' | 'sage' | 'olive' | 'sand' | 'gold' | 'bronze' |
-    'brown' | 'yellow' | 'amber' | 'orange' | 'tomato' | 'red' | 'ruby' | 'crimson' |
-    'pink' | 'plum' | 'purple' | 'violet' | 'iris' | 'indigo' | 'blue' | 'cyan' |
-    'teal' | 'jade' | 'green' | 'grass' | 'lime' | 'mint' | 'sky'
-
+  'gray' | 'mauve' | 'slate' | 'sage' | 'olive' | 'sand' | 'gold' | 'bronze' |
+  'brown' | 'yellow' | 'amber' | 'orange' | 'tomato' | 'red' | 'ruby' | 'crimson' |
+  'pink' | 'plum' | 'purple' | 'violet' | 'iris' | 'indigo' | 'blue' | 'cyan' |
+  'teal' | 'jade' | 'green' | 'grass' | 'lime' | 'mint' | 'sky' | 'black' | 'white'
 
 export interface ColorPaletteProps {
-    palette: palette;
+  palette: palette
 }
 
 export const defaults: Partial<ColorPaletteProps> = {
-    palette: 'gray',
-};
+  palette: 'gray',
+}
 
 export const allPalettes: palette[] = [
-    'gray', 'mauve', 'slate', 'sage', 'olive', 'sand', 'gold', 'bronze',
-    'brown', 'yellow', 'amber', 'orange', 'tomato', 'red', 'ruby', 'crimson',
-    'pink', 'plum', 'purple', 'violet', 'iris', 'indigo', 'blue', 'cyan',
-    'teal', 'jade', 'green', 'grass', 'lime', 'mint', 'sky'
-];
+  'gray',
+  'mauve',
+  'slate',
+  'sage',
+  'olive',
+  'sand',
+  'gold',
+  'bronze',
+  'brown',
+  'yellow',
+  'amber',
+  'orange',
+  'tomato',
+  'red',
+  'ruby',
+  'crimson',
+  'pink',
+  'plum',
+  'purple',
+  'violet',
+  'iris',
+  'indigo',
+  'blue',
+  'cyan',
+  'teal',
+  'jade',
+  'green',
+  'grass',
+  'lime',
+  'mint',
+  'sky',
+  'black',
+  'white',
+]
