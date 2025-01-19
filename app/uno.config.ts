d04d75323838634af098568159cc9ebe928e876c
Flame <teke900@gmail.com>
2025-01-19 18:57:54 +0100

    feat: Refactor uno.config.ts to enhance theme and presets.
    Replaced `theme` with `extendTheme` to simplify customization and added `pureBlack` and `pureWhite` to colors. Updated presetRadix to include `black` and `white`. Streamlined formatting for improved readability and consistency.

--- app/uno.config.ts
+++ app/uno.config.ts
@@ -8,76 +8,79 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
-import {presetRadix} from 'unocss-preset-radix'
-import {safelist} from './assets/uno/scales'
+import { presetRadix } from 'unocss-preset-radix'
+import { safelist } from './assets/uno/scales'
 
 export default defineConfig({
-    shortcuts: [
-        // Define any shortcuts here
-    ],
-    theme: {
-        colors: {
-            // Extend or override theme colors here if needed
-        },
-        extend: {
-            // Custom Fonts
-            // TODO: Add custom pattern for fonts
-            fontFamily: {
-                chopin: ['Chopin Light', 'sans-serif'],
-            },
-        },
-    },
-
-    presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons(),
-        presetTypography(),
-        presetWebFonts({
-            fonts: {
-                // Define your custom web fonts here if needed
-            },
-        }),
-        presetRadix({
-            palette: [
-                'gray',
-                'mauve',
-                'slate',
-                'sage',
-                'olive',
-                'sand',
-                'gold',
-                'bronze',
-                'brown',
-                'yellow',
-                'amber',
-                'orange',
-                'tomato',
-                'red',
-                'ruby',
-                'crimson',
-                'pink',
-                'plum',
-                'purple',
-                'violet',
-                'iris',
-                'indigo',
-                'blue',
-                'cyan',
-                'teal',
-                'jade',
-                'green',
-                'grass',
-                'lime',
-                'mint',
-                'sky',
-            ],
-            extend: false,
-        }),
-    ],
-    safelist,
-    transformers: [
-        transformerDirectives(),
-        transformerVariantGroup(),
-    ],
+  shortcuts: [
+    // Define any shortcuts here
+  ],
+  extendTheme: (theme) => {
+    return {
+      ...theme,
+      colors: {
+        ...theme.colors,
+        pureBlack: '#000000', // Add pure black
+        pureWhite: '#FFFFFF', // Add pure white
+      },
+      // Custom Fonts
+      // TODO: Add custom pattern for fonts
+      fontFamily: {
+        chopin: ['Chopin Light', 'sans-serif'],
+      },
+    }
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // Define your custom web fonts here if needed
+      },
+    }),
+    presetRadix({
+      palette: [
+        'gray',
+        'mauve',
+        'slate',
+        'sage',
+        'olive',
+        'sand',
+        'gold',
+        'bronze',
+        'brown',
+        'yellow',
+        'amber',
+        'orange',
+        'tomato',
+        'red',
+        'ruby',
+        'crimson',
+        'pink',
+        'plum',
+        'purple',
+        'violet',
+        'iris',
+        'indigo',
+        'blue',
+        'cyan',
+        'teal',
+        'jade',
+        'green',
+        'grass',
+        'lime',
+        'mint',
+        'sky',
+        'black',
+        'white',
+      ],
+    }),
+  ],
+  safelist,
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 })
